03571de26b54efe4bf8378820cf5736cbefcc7b9
neoteric <asampathkumar545@gmail.com>
2024-07-06 15:56:51 +0530

    commit

---
+++ src/test/java/com/neoteric/movieticket/MovieTicketTest.java
@@ -0,0 +1,20 @@
+package com.neoteric.movieticket;
+
+import MovieTicket222.MovieTicket25;
+import org.junit.jupiter.api.Test;
+
+public class MovieTicketTest {
+    @Test
+
+    public void test(){
+
+        MovieTicket25 ticket25=new MovieTicket25();
+        ticket25.moviename="kalki";
+        ticket25.price=222;
+        ticket25.date=21-5-2024;
+        ticket25.row=25l;
+        ticket25.seatnumber=28;
+        System.out.println("MovieTicket");
+
+    }
+}
